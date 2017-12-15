fbe5897b79a333c9e4ffdeb58e5df3281112a9ce
bbarsi <balazs.barsi@apex-it-services.eu>
2017-12-15 15:56:02 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+ exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
